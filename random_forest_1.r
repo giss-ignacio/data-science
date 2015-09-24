6f523fe19f5580f4bcef5722b1118ba7788a208c
Ignacio Giss <giss.ignacio@gmail.com>
2015-09-24 09:38:33 -0300

    dropping levels
    dropping levels

--- random_forest_1.r
+++ random_forest_1.r
@@ -10,7 +10,7 @@ test <- read.csv("test2.csv")
 splitdf <- function(dataframe, seed=NULL) {
 	if (!is.null(seed)) set.seed(seed)
 	index <- 1:nrow(dataframe)
-	trainindex <- sample(index, trunc(length(index)/3))
+	trainindex <- sample(index, trunc(length(index)/100))
 	trainset <- dataframe[trainindex, ]
 	testset <- dataframe[-trainindex, ]
 	list(trainset=trainset,testset=testset)
@@ -22,4 +22,13 @@ train2 <- splits$trainset
 
 library(randomForest)
 
+train2 <- droplevels(train2)
 my_forest <- randomForest(Category ~ PdDistrict + DayOfWeek + Dates + X + Y + Rain  + Tmin + Tmax + Tavrg, data=train2, importance=TRUE, ntree=1000)
+
+my_prediction <- predict(my_forest, test)
+
+# Create a data frame with two columns: PassengerId & Survived. Survived contains your predictions
+my_solution <- data.frame(PassengerId = test$PassengerId, Survived = my_prediction)
+
+# Write your solution away to a csv file with the name my_solution.csv
+write.csv(my_solution, file = "my_solution.csv", row.names = FALSE)
