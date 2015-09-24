389809d16811e83b836b94b432a40e305c1b61a9
Ignacio Giss <giss.ignacio@gmail.com>
2015-09-24 07:28:51 -0300

    correccion en los csv
    correccion en los csv

--- partir_train_R.r
+++ partir_train_R.r
@@ -6,7 +6,7 @@ test <- read.csv("test.csv")
 splitdf <- function(dataframe, seed=NULL) {
 	if (!is.null(seed)) set.seed(seed)
 	index <- 1:nrow(dataframe)
-	trainindex <- sample(index, trunc(length(index)*2/3))
+	trainindex <- sample(index, trunc(length(index)/2))
 	trainset <- dataframe[trainindex, ]
 	testset <- dataframe[-trainindex, ]
 	list(trainset=trainset,testset=testset)
